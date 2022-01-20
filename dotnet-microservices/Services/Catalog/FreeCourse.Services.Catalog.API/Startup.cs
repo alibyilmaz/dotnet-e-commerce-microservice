ab879deb31301a5038479e4d452626f2a83deb5c
ali <aliburak.yilmaz@outlook.com>
2022-01-21 01:41:57 +0300

    add automapper and mapping class

--- dotnet-microservices/Services/Catalog/FreeCourse.Services.Catalog.API/Startup.cs
+++ dotnet-microservices/Services/Catalog/FreeCourse.Services.Catalog.API/Startup.cs
@@ -31,6 +31,7 @@ namespace FreeCourse.Services.Catalog.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.Catalog.API", Version = "v1" });
             });
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
