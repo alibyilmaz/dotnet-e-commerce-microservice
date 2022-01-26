4e401711c662cfcfd2a663b7fadc0a7f3c992e32
ali <aliburak.yilmaz@outlook.com>
2022-01-27 00:35:38 +0300

    add IBasketService interface

---
+++ dotnet-microservices/Services/Basket/FreeCourse.Services.Basket.API/Services/IBasketService.cs
@@ -0,0 +1,17 @@
+﻿using FreeCourse.Services.Basket.API.Dtos;
+using FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Basket.API.Services
+{
+   public interface IBasketService
+    {
+        Task<ResponseDto<BasketDto>> GetBasket(string userId);
+
+        Task<ResponseDto<bool>> SaveOrUpdate(BasketDto basketDto);
+        Task<ResponseDto<bool>> Delete(string userId);
+    }
+}
