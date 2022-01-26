50df7e2bd9d6d62a92d387ea58a4202d35aa48fc
ali <aliburak.yilmaz@outlook.com>
2022-01-27 00:50:28 +0300

    add crud service basket api

---
+++ dotnet-microservices/Services/Basket/FreeCourse.Services.Basket.API/Services/BasketService.cs
@@ -0,0 +1,43 @@
+﻿using FreeCourse.Services.Basket.API.Dtos;
+using FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Basket.API.Services
+{
+    public class BasketService : IBasketService
+    {
+        private readonly RedisService _redisService;
+
+        public BasketService(RedisService redisService)
+        {
+            _redisService = redisService;
+        }
+        public async Task<ResponseDto<bool>> Delete(string userId)
+        {
+            var status = await _redisService.GetDatabase().KeyDeleteAsync(userId);
+            return status ? ResponseDto<bool>.Success(204) : ResponseDto<bool>.Fail("not found", 404);
+        }
+
+        public async Task<ResponseDto<BasketDto>> GetBasket(string userId)
+        {
+            var existBasket = await _redisService.GetDatabase().StringGetAsync(userId);
+
+            if (String.IsNullOrEmpty(existBasket))
+            {
+                return ResponseDto<BasketDto>.Fail("not found", 404);
+            }
+            return ResponseDto<BasketDto>.Success(JsonSerializer.Deserialize<BasketDto>(existBasket), 200); 
+        }
+
+        public async Task<ResponseDto<bool>> SaveOrUpdate(BasketDto basketDto)
+        {
+            var status = await _redisService.GetDatabase().StringSetAsync(basketDto.UserId, JsonSerializer.Serialize(basketDto));
+
+            return status ? ResponseDto<bool>.Success(204) : ResponseDto<bool>.Fail("could not save or update",500);
+        }
+    }
+}
