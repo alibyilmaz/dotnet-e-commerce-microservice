daab6c02218577faae312b9dc654682069909010
ali <aliburak.yilmaz@outlook.com>
2022-02-03 23:21:14 +0300

    add discount service crud operations

--- dotnet-microservices/Services/Discount/FreeCourse.Services.Discount.API/Services/DiscountService.cs
+++ dotnet-microservices/Services/Discount/FreeCourse.Services.Discount.API/Services/DiscountService.cs
@@ -1,6 +1,10 @@
-﻿using FreeCourse.Shared.Dtos;
+﻿using Dapper;
+using FreeCourse.Shared.Dtos;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,34 +12,78 @@ namespace FreeCourse.Services.Discount.API.Services
 {
     public class DiscountService : IDiscountService
     {
-        public Task<ResponseDto<NoContent>> Delete(int id)
+        private readonly IConfiguration _configuration;
+        private readonly IDbConnection _dbConnection;
+
+        public DiscountService(IConfiguration configuration)
         {
-            throw new NotImplementedException();
+            _configuration = configuration;
+
+            _dbConnection = new NpgsqlConnection(_configuration.GetConnectionString("PostgreSql"));
+        }
+        public async Task<ResponseDto<NoContent>> Delete(int id)
+        {
+            var status = await _dbConnection.ExecuteAsync("delete from discount id=@Id", new { Id = id });
+
+            return status > 0 ? ResponseDto<NoContent>.Success(204) : ResponseDto<NoContent>.Fail("not found",404);
         }
 
-        public Task<ResponseDto<List<Models.Discount>>> GetAll()
+        public async Task<ResponseDto<List<Models.Discount>>> GetAll()
         {
-            throw new NotImplementedException();
+            var discount = await _dbConnection.QueryAsync<Models.Discount>("Select * from discount");
+            return ResponseDto<List<Models.Discount>>.Success(discount.ToList(), 200);
         }
 
-        public Task<ResponseDto<Models.Discount>> GetByCodeAndUserId(string code, string userId)
+        public async Task<ResponseDto<Models.Discount>> GetByCodeAndUserId(string code, string userid)
         {
-            throw new NotImplementedException();
+            var discount = await _dbConnection.QueryAsync<Models.Discount>("Select * from discount where userid=@UserId and code=@Code", new { UserId = userid, Code=code});
+            var hashDiscount = discount.FirstOrDefault();
+            
+            if (hashDiscount == null)
+            {
+                return ResponseDto<Models.Discount>.Fail("not found", 404);
+            }
+            return ResponseDto<Models.Discount>.Success(hashDiscount, 200);
         }
 
-        public Task<ResponseDto<Models.Discount>> GetById(int id)
+        public async Task<ResponseDto<Models.Discount>> GetById(int id)
         {
-            throw new NotImplementedException();
+            var discount = (await _dbConnection.QueryAsync<Models.Discount>("Select * from discount where id=@Id",new {Id = id }))
+                .SingleOrDefault();
+            if (discount==null)
+            {
+                return ResponseDto<Models.Discount>.Fail("not found", 404);
+            }
+            return ResponseDto<Models.Discount>.Success(discount, 200);
+
+
+
         }
 
-        public Task<ResponseDto<NoContent>> Save(Models.Discount discount)
+        public async Task<ResponseDto<NoContent>> Save(Models.Discount discount)
         {
-            throw new NotImplementedException();
+            var status = await _dbConnection.ExecuteAsync("INSERT INTO discount(userid,rate,code) VALUES(@UserId,@Rate,@Code)",discount);
+
+            if (status>0)
+            {
+                return ResponseDto<NoContent>.Success(204);
+            }
+
+            return ResponseDto<NoContent>.Fail("error occured",400);
+
         }
 
-        public Task<ResponseDto<NoContent>> Update(Models.Discount discount)
+        public async Task<ResponseDto<NoContent>> Update(Models.Discount discount)
         {
-            throw new NotImplementedException();
+            var status = await _dbConnection.ExecuteAsync("update discount set userid=@UserId,code=@Code,rate=@Rate where id=@Id",new { 
+            
+            Id=discount.Id, UserId=discount.UserId,Code=discount.Code,Rate=discount.Rate
+            });
+            if (status > 0)
+            {
+                return ResponseDto<NoContent>.Success(204);
+            }
+            return ResponseDto<NoContent>.Fail("error occured", 400);
         }
     }
 }
