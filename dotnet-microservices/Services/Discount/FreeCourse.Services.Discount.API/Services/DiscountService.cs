b226085173e648ba92647506516007810b3efd9f
ali <aliburak.yilmaz@outlook.com>
2022-02-03 22:57:08 +0300

    add discount service

---
+++ dotnet-microservices/Services/Discount/FreeCourse.Services.Discount.API/Services/DiscountService.cs
@@ -0,0 +1,41 @@
+﻿using FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeCourse.Services.Discount.API.Services
+{
+    public class DiscountService : IDiscountService
+    {
+        public Task<ResponseDto<NoContent>> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto<List<Models.Discount>>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto<Models.Discount>> GetByCodeAndUserId(string code, string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto<Models.Discount>> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto<NoContent>> Save(Models.Discount discount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseDto<NoContent>> Update(Models.Discount discount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
