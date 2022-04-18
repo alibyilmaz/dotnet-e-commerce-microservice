37d6abb125b93b9b35264ea669444a74bd75d46b
ali <aliburak.yilmaz@outlook.com>
2022-04-19 00:48:50 +0300

    add mediatr and queryhandlers

---
+++ dotnet-microservices/Services/Order/FreeCouse.Services.Order.Application/Queries/GetOrdersByUserIdQuery.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreeCourse.Shared.Dtos;
+using FreeCouse.Services.Order.Application.Dtos;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FreeCourse.Services.Order.Application.Queries
+{
+     class GetOrdersByUserIdQuery : IRequest<ResponseDto<List<OrderDto>>>
+    {
+        public string UserId { get; set; }
+    }
+}
