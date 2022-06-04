4f70a93a5c80ee2063c8bf720d18782d9da4734b
ali <aliburak.yilmaz@outlook.com>
2022-06-04 23:04:26 +0300

    add payment api and configure identity

---
+++ dotnet-microservices/Services/FakePayment/FreeCourse.Services.FakePayment/Controllers/FakePaymentsController.cs
@@ -0,0 +1,24 @@
+﻿using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FreeCourse.Shared.ControllerBases;
+using FreeCourse.Shared.Services;
+
+namespace FreeCourse.Services.FakePayment.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FakePaymentsController : CustomBaseController
+    {
+
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance(ResponseDto<NoContent>.Success(200));
+        }
+    }
+}
