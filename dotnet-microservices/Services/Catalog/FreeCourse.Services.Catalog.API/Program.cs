b61d7bddbfc352a022420c8ecb0652fa83f755b4
ali <aliburak.yilmaz@outlook.com>
2022-06-05 00:42:21 +0300

    add all apis gateway

--- dotnet-microservices/Services/Catalog/FreeCourse.Services.Catalog.API/Program.cs
+++ dotnet-microservices/Services/Catalog/FreeCourse.Services.Catalog.API/Program.cs
@@ -6,6 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FreeCourse.Services.Catalog.API.Dtos;
+using FreeCourse.Services.Catalog.API.Services;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace FreeCourse.Services.Catalog.API
 {
@@ -13,7 +16,20 @@ namespace FreeCourse.Services.Catalog.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+          var host =  CreateHostBuilder(args).Build();
+          using (var scope = host.Services.CreateScope())
+          {
+              var servcieProvider = scope.ServiceProvider;
+              var categoryService = servcieProvider.GetRequiredService<ICategoryService>();
+
+              if (!categoryService.GetAllAsync().Result.Data.Any())
+              {
+                  categoryService.CreateAsync((new CategoryDto() {Name = "Asp Net Core Kursu"})).Wait();
+                  categoryService.CreateAsync((new CategoryDto() {Name = "Asp Net Core API Kursu"})).Wait();
+              }
+
+          }
+          host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
