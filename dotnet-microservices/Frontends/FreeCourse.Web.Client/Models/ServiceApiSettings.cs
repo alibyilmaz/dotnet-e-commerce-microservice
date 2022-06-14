5218c7e80db0031b4677f584e26c7a5f835f70bb
ali <aliburak.yilmaz@outlook.com>
2022-06-15 01:19:18 +0300

    add catalog service web startup

--- dotnet-microservices/Frontends/FreeCourse.Web.Client/Models/ServiceApiSettings.cs
+++ dotnet-microservices/Frontends/FreeCourse.Web.Client/Models/ServiceApiSettings.cs
@@ -10,5 +10,11 @@ namespace FreeCourse.Web.Client.Models
         public string IdentityBaseUri { get; set; }
         public string GatewayBaseUri { get; set; }
         public string PhotoStockUri { get; set; }
+        public ServiceApi Catalog { get; set; }
+    }
+
+    public class ServiceApi
+    {
+        public string Path { get; set; }
     }
 }
