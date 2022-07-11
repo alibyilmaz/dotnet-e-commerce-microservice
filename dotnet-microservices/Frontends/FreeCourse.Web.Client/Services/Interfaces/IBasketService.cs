f439cc49151d3def6b2ee8a58c5101b7fb730e93
ali <aliburak.yilmaz@outlook.com>
2022-07-12 00:27:37 +0300

    add basket service

---
+++ dotnet-microservices/Frontends/FreeCourse.Web.Client/Services/Interfaces/IBasketService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using FreeCourse.Web.Client.Models.Baskets;
+
+namespace FreeCourse.Web.Client.Services.Interfaces
+{
+    public interface IBasketService
+    {
+        Task<bool> SaveOrUpdate(BasketViewModel basketViewModel);
+
+        Task<BasketViewModel> GetBasketAsync();
+
+        Task<bool> DeleteBasketAsync();
+
+        Task AddBasketItem(BasketViewModel basketViewModel);
+
+        Task<bool> RemoveBasketItem(string courseId);
+
+        Task<bool> ApplyDiscount(string discountCode);
+        Task<bool> CancelApplyDiscount();
+    }
+}
